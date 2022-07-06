196a477290d564d3f660c5830e4edae594e8021d
Thomas Berinde <thomasberinde@Thomass-MacBook-Pro.local>
2022-07-06 14:40:19 +0300

    191: Add 3 students and specify auto incremet rule

--- src/com/rodeapps/hibernate/demo/PrimaryKeyDemo.java
+++ src/com/rodeapps/hibernate/demo/PrimaryKeyDemo.java
@@ -1,13 +1,10 @@
 package com.rodeapps.hibernate.demo;
 
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class CreateStudentDemo {
-
-    // This inserts a student into the database
+public class PrimaryKeyDemo {
 
     public static void main(String[] args) {
 
@@ -23,16 +20,20 @@ public class CreateStudentDemo {
         try {
             // use the session object to save Java object
 
-            // create a student object
-            System.out.println("Creating a new student object");
-            Student tempStudent = new Student("Paul", "Wall", "paul@rodeapps.com");
+            // create 3 student objects
+            System.out.println("Creating 3 student objects");
+            Student stud1 = new Student("John", "Doe", "john@rodeapps.com");
+            Student stud2 = new Student("Mary", "Public", "mary@rodeapps.com");
+            Student stud3 = new Student("Bonita", "Applebum", "bonita@rodeapps.com");
 
             // start transaction
             session.beginTransaction();
 
             // save the student object
-            System.out.println("Saving the student...");
-            session.save(tempStudent);
+            System.out.println("Saving the students...");
+            session.save(stud1);
+            session.save(stud2);
+            session.save(stud3);
 
             // commit transaction
             session.getTransaction().commit();
@@ -40,7 +41,7 @@ public class CreateStudentDemo {
             System.out.println("Done!");
         }
         finally {
-             factory.close();
+            factory.close();
         }
     }
 }
